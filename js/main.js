7ff71d313478aba3b61452106676e341ee3634cb
Daniel Caneda <daniel.caneda@loop.colum.edu>
2018-03-05 11:41:02 -0600

    updated navbar

--- js/main.js
+++ js/main.js
@@ -191,7 +191,24 @@ document.querySelector('.prev').addEventListener('click', function() {
      counter--;
      showCurrent();
   }, false);
-  
+	
+	
+ $(function() {
+       $('a[href*="#"]:not([href="#"])').click(function() {
+         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+           var target = $(this.hash);
+           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html, body').animate({
+               scrollTop: target.offset().top
+             }, 1000);
+             return false;
+           }
+         }
+       });
+     });
+
+
 })();  
 
 
